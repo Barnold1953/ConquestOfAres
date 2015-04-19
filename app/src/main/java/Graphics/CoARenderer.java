f6b98b26c35a49eea330904283c28eec30bf5818
Nathan Matthews <nmatthe5@utk.edu>
2015-04-19 18:22:14 -0400

    debug stuff

--- app/src/main/java/Graphics/CoARenderer.java
+++ app/src/main/java/Graphics/CoARenderer.java
@@ -116,7 +116,12 @@ public class CoARenderer implements GLSurfaceView.Renderer {
 
         float[] slope = new float[2];
 
+        /*if(u.destination != u.location){
+            Log.d("slope", "destination: (" + u.destination.x + ", " + u.destination.y + ") location: (" + u.location.x + ", " + u.location.y + ")");
+            Log.d("slope", "width: " + width + ", height: " + height);
+        }*/
         if(Math.abs(u.destination.x - u.location.x) > width / 3){
+            //Log.d("slope", "horizontal wrap");
             slope[0] = -(u.destination.x - u.location.x) / u.speed;
             if(u.wrapFrame.x == -1){
                 for(int i = 0; i < u.speed; i++){
@@ -139,6 +144,7 @@ public class CoARenderer implements GLSurfaceView.Renderer {
         }
 
         if(Math.abs(u.destination.y - u.location.y) > height / 3){
+            Log.d("slope", "vertical wrap");
             slope[1] = -(u.destination.y - u.location.y) / u.speed;
             if(u.wrapFrame.y == -1){
                 for(int i = 0; i < u.speed; i++){
